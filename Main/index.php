a093ad2ae0e0e842dbab7016b20957c80c028a2d
sdlitwi <scottbot84@gmail.com>
2011-11-01 13:03:59 -0500

    fix for google login (again)

--- Main/index.php
+++ Main/index.php
@@ -33,16 +33,21 @@ $yourfield = 'mileage';
 mysql_connect($hostname,$username, $password) OR DIE ('Unable to connect to database! Please try again later.');
 mysql_select_db($dbname);
 
-//Example Query
+			// Add User to Database
+					if (isset($_POST['first'])) {
+						$first = $_POST['first'];
+						$last = $_POST['last'];
+						$phone = $_POST['phone'];
+						$age = $_POST['age'];
+						$weight = $_POST['weight'];
+						$prefemail = $_POST['prefemail'];
+						$email = $_POST['email'];
 
-// $query = 'SELECT * FROM ' . $usertable;
-// $result = mysql_query($query);
-// if($result) {
-    // while($row = mysql_fetch_array($result)){
-        // $name = $row[$yourfield];
-        // echo 'Name: ' . $name;
-    // }
-// }
+						$query = 'INSERT INTO USER (firstName,lastName,phone,loginEmail,prefferedEmail,age,weight,isAdmin) VALUES("' . $first . '","' . $last . '",' . $phone . ',"' . $email . '","' . $prefemail . '",' . $age . ',' . $weight . ',0' . ');';
+						echo $query;
+						$result = mysql_query($query);
+						$registered = true;
+					}
 
 // //Twitter PHP Class (uncomment to use)
 
@@ -253,22 +258,7 @@ $email = $_REQUEST['openid_ext1_value_email'];
 						$registered = true;
 					}
 				}
-			// Add User to Database
-					if (isset($_POST['first'])) {
-						$first = $_POST['first'];
-						$last = $_POST['last'];
-						$phone = $_POST['phone'];
-						$age = $_POST['age'];
-						$weight = $_POST['weight'];
-						$prefemail = $_POST['email'];
-
-						$query = 'INSERT INTO USER (firstName,lastName,phone,loginEmail,prefferedEmail,age,weight,isAdmin) VALUES("' . $first . '","' . $last . '",' . $phone . ',"' . $email . '","' . $prefemail . '",' . $age . ',' . $weight . ',0' . ');';
-						echo $query;
-						$result = mysql_query($query);
-						// add check to see if user was added successfully
-						$registered = true;
-					}
-				}
+}
 		?>
 		<!-- Facebook PHP Code-->
 		<?php
@@ -295,27 +285,15 @@ $email = $_REQUEST['openid_ext1_value_email'];
 						$id = $row['userid'];
 						$registered = true;
 					}
-					
-			// Add User to Database
-					if (isset($_POST['first'])) {
-						$first = $_POST['first'];
-						$last = $_POST['last'];
-						$phone = $_POST['phone'];
-						$age = $_POST['age'];
-						$weight = $_POST['weight'];
-						$prefemail = $_POST['email'];
-
-						$query = 'INSERT INTO USER (firstName,lastName,phone,loginEmail,prefferedEmail,age,weight,isAdmin) VALUES("' . $first . '","' . $last . '",' . $phone . ',"' . $email . '","' . $prefemail . '",' . $age . ',' . $weight . ',0' . ');';
-						echo $query;
-						$result = mysql_query($query);
-						$registered = true;
-					}
+			
 				}
 			} catch(Exception $o) {
 				d($o);
 			}
 
 		}
+				
+
 
 		// Add status using graph api
 		// if (isset($_POST['status'])) {
@@ -544,9 +522,10 @@ $email = $_REQUEST['openid_ext1_value_email'];
 								</p>
 								<h3 style="padding-top:20px">Contact Information</h3>
 								<p>
-									E-mail:
-									<input type="text" class="search"  id="email" name="email" value="<?php print $email ?>" />
+									Contact E-mail:
+									<input type="text" class="search"  id="prefemail" name="prefemail" value="<?php print $email ?>" />
 								</p>
+								<input type="hidden" name="email" id = "email" value="<?php print $email ?>">
 								<input type="submit" value="Register"/>
 								<br/>
 								<br/>
