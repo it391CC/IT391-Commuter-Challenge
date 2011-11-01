ef7fe7a9f633f78569c712ca9cae49896083c698
Shawn Rhoney <smrhoney@gmail.com>
2011-10-31 19:11:12 -0500

    Google and Facebook use case

---
+++ android/AndroidProject/src/droid/android2/OAuth2GoogleCredentials.java
@@ -0,0 +1,29 @@
+package droid.android2;
+
+/**
+* OAuth 2 credentials found in the <a
+* href="https://code.google.com/apis/console">Google apis console</a>.
+*/
+public class OAuth2GoogleCredentials {
+
+
+
+	/** Value of the "Client ID" shown under "Client ID for installed applications". */
+	public static final String CLIENT_ID = "694982572573.apps.googleusercontent.com";
+
+	/** Value of the "Client secret" shown under "Client ID for installed applications". */
+	public static final String CLIENT_SECRET = "siVHat93oGK_H43Yr0UIrb55";
+
+	/** OAuth 2 scope to use */
+	public static final String SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+
+	/** OAuth 2 redirect uri */
+	public static final String REDIRECT_URI = "http://localhost";
+
+	/** Latitude API key */
+	public static final String API_KEY = "AIzaSyAPEJ3PXUJEOM9P1nwXtmLKsu1J2FuaTsI";
+
+	
+
+
+}
