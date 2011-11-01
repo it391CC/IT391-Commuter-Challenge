f9f2d54ed17941c3d8abdb0d58d821814c823deb
unknown <Scott@Gypsy.(none)>
2011-10-31 20:37:29 -0500

    fixed repo (hopefully)

---
+++ Android/AndroidProject/src/com/store/CredentialStore.java
@@ -0,0 +1,10 @@
+package com.store;
+
+import com.google.api.client.auth.oauth2.draft10.AccessTokenResponse;
+
+public interface CredentialStore {
+
+  AccessTokenResponse read();
+  void write(AccessTokenResponse response);
+  void clearCredentials();
+}
