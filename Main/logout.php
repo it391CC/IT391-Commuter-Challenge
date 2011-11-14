e5dcf7e49045132e7196081937e94191f2245605
Scott Litwiller <sdlitwi@gmail.com>
2011-11-13 23:27:26 -0600

    fix for login, added logout.php

---
+++ Main/logout.php
@@ -0,0 +1,16 @@
+<?php
+//Google/PHP Session
+session_start();
+$_SESSION['loggedin']=false;
+$email = $_SESSION['user']=false;
+$_SESSION['admin']=false;
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+xmlns:fb="http://www.facebook.com/2008/fbml">
+	<head>
+	</head>
+	<body>
+	</body>
+	<meta HTTP-EQUIV="REFRESH" content="0; url=http://limbotestserver.com">
+</html>
